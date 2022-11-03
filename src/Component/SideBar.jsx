c206e73646a716facdb7a5b1db6a43fd6e549e74
farah <farahallouche327@gmail.com>
2022-11-03 13:12:52 +0100

    fix side bar

--- src/Component/SideBar.jsx
+++ src/Component/SideBar.jsx
@@ -15,44 +15,46 @@ export default function SideBar({ setSideBar, sideBar }) {
 
   console.log("re render sidebar");
   return (
-    <IconContext.Provider value={{ color: "#ede7e0", size: "25px" }}>
-      <div
-        className={
-          sideBar == 0
-            ? "side-bar hidden"
-            : sideBar == 1
-            ? "side-bar show"
-            : "side-bar hide"
-        }
-      >
-        <div className="quit-icon">
-          <span onClick={hideSideBare}>
-            <BsXSquare style={{ color: "#ede7e0", size: "40" }}></BsXSquare>
-          </span>
-        </div>
-        <Link to="/account">
-          <div className="profile-info">
-            <BadgeAvatars photo={photo} />
-            <h3>{userName}</h3>
+    <div className={sideBar == 1 ? "bg-modal" : ""}>
+      <IconContext.Provider value={{ color: "#ede7e0", size: "25px" }}>
+        <div
+          className={
+            sideBar == 0
+              ? "side-bar hidden"
+              : sideBar == 1
+              ? "side-bar show"
+              : "side-bar hide"
+          }
+        >
+          <div className="quit-icon">
+            <span onClick={hideSideBare}>
+              <BsXSquare style={{ color: "#ede7e0", size: "40" }}></BsXSquare>
+            </span>
           </div>
-        </Link>
-        <div className="links-container">
-          <ul>
-            <Link to="/adopt">
-              <li>Adopt new pet</li>
-            </Link>
-            <Link to="/rehome">
-              <li>Rehome your pet</li>
-            </Link>
-            <Link to="/lostPets">
-              <li>Lost and found</li>
-            </Link>
-          </ul>
+          <Link to="/account">
+            <div className="profile-info">
+              <BadgeAvatars photo={photo} />
+              <h3>{userName}</h3>
+            </div>
+          </Link>
+          <div className="links-container">
+            <ul>
+              <Link to="/adopt">
+                <li>Adopt new pet</li>
+              </Link>
+              <Link to="/rehome">
+                <li>Rehome your pet</li>
+              </Link>
+              <Link to="/lostPets">
+                <li>Lost and found</li>
+              </Link>
+            </ul>
+          </div>
+          <div className="img-dog">
+            <img src="../images/dog.png"></img>
+          </div>
         </div>
-        <div className="img-dog">
-          <img src="../images/dog.png"></img>
-        </div>
-      </div>
-    </IconContext.Provider>
+      </IconContext.Provider>
+    </div>
   );
 }
